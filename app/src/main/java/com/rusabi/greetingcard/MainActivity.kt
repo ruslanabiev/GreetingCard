5c360e1f1c20607cc028a0f71920368b6048255c
Ruslan Abiev <abievruslan@yandex.ru>
2022-05-19 19:18:05 +0300

    Creating project

--- app/src/main/java/com/rusabi/greetingcard/MainActivity.kt
+++ app/src/main/java/com/rusabi/greetingcard/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rusabi.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContent {
+
             GreetingCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -31,13 +38,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+    Surface(color = Color.Magenta) {
+
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = Modifier.padding(24.dp)
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GreetingCardTheme {
-        Greeting("Android")
+        Greeting("Meghan")
     }
 }
